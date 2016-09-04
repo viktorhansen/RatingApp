ab14bbaa21863bb6e172b6c1c6de12150aab5a7b
Viktor Hansen <viktorhansen@me.com>
2016-09-04 18:10:28 +0200

    Skapat ett enkelt API med CRUD, statisk lista för persistering av data än så länge

---
+++ src/RatingApp/Controllers/RatingsController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RatingApp.Models;
+using Newtonsoft.Json;
+
+namespace RatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class RatingsController : Controller
+    {
+        DataManager _dataManager = new DataManager();
+
+        // GET api/ratings
+        [HttpGet]
+        public IEnumerable<Rating> Get()
+        {
+            return _dataManager.GetAll();
+        }
+
+        // GET api/ratings/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var item = _dataManager.GetById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // POST api/ratings
+        [HttpPost]
+        public IActionResult Post([FromBody]Rating item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            _dataManager.Add(item);
+            return CreatedAtRoute("Ratings", new { id = item.Id }, item);
+        }
+
+        // PUT api/ratings/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Rating item)
+        {
+            if (item == null || item.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var tmpItem = _dataManager.GetById(id);
+            if (tmpItem == null)
+            {
+                return NotFound();
+            }
+
+            _dataManager.Update(item);
+            return new NoContentResult();
+        }
+
+        // DELETE api/ratings/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _dataManager.GetById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _dataManager.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
